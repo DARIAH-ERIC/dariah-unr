620bb33da9deef5b41f24ad250932f5fc5daffa9
Stefan Probst <stefanprobst@posteo.de>
2024-03-12 15:13:55 +0100

    feat: add lead-in text to form steps [skip ci]

--- app/[locale]/dashboard/reports/[year]/countries/[code]/edit/[step]/page.tsx
+++ app/[locale]/dashboard/reports/[year]/countries/[code]/edit/[step]/page.tsx
@@ -19,6 +19,7 @@ import { PublicationsForm } from "@/components/forms/publications-form";
 import { ReportSummary } from "@/components/forms/report-summary";
 import { ServiceReportsForm } from "@/components/forms/service-reports-form";
 import { SoftwareForm } from "@/components/forms/software-form";
+import { Link } from "@/components/link";
 import { MainContent } from "@/components/main-content";
 import { PageTitle } from "@/components/page-title";
 import type { Locale } from "@/config/i18n.config";
@@ -146,11 +147,28 @@ async function DashboardCountryReportEditStepPageContent(
 				<section className="grid gap-6">
 					<FormTitle>{t("contributions")}</FormTitle>
 					<FormDescription>
-						Tempor aliqua non ad sint sint officia Lorem cupidatat cillum sint non tempor quis qui.
-						Voluptate sunt cillum et do cillum ut deserunt aliqua nisi pariatur velit adipisicing
-						sint. Incididunt laboris tempor cupidatat tempor laborum nulla consectetur consectetur
-						exercitation tempor veniam ullamco ullamco commodo. Nulla commodo ad dolor laboris nulla
-						tempor labore.
+						<p>
+							Here we want to capture the estimated number of individuals contributing to DARIAH
+							within the national node. You should include anyone who participates regularly in
+							national DARIAH events, contributes to national initiatives or projects, acts as a
+							contact point within an institution, receives national or institutional funding linked
+							to DARIAH in your country. Categories that might be applicable in your country:
+						</p>
+
+						<ol>
+							<li>At least one person per partner institution</li>
+							<li>Anyone directly paid or funded by the a national DARIAH (or CLARIAH) project</li>
+							<li>People on &quot;DARIAH-affiliated projects&quot; (humanities infrastructure)</li>
+							<li>People&apos;s whose work is submitted as DARIAH in kind contribution</li>
+							<li>
+								People making contribution but who are not paid (running DARIAH-relevant events)
+							</li>
+						</ol>
+
+						<p>
+							This response will consist of a single number. If you estimate, please let us know the
+							basis for your estimate, using the comment section.
+						</p>
 					</FormDescription>
 
 					<ContributionsForm
@@ -171,11 +189,29 @@ async function DashboardCountryReportEditStepPageContent(
 				<section className="grid gap-6">
 					<FormTitle>{t("events")}</FormTitle>
 					<FormDescription>
-						Tempor aliqua non ad sint sint officia Lorem cupidatat cillum sint non tempor quis qui.
-						Voluptate sunt cillum et do cillum ut deserunt aliqua nisi pariatur velit adipisicing
-						sint. Incididunt laboris tempor cupidatat tempor laborum nulla consectetur consectetur
-						exercitation tempor veniam ullamco ullamco commodo. Nulla commodo ad dolor laboris nulla
-						tempor labore.
+						<p>This response will consist of three numbers:</p>
+
+						<ul>
+							<li>number of large meetings (more than 50 people in attendance),</li>
+							<li>number of medium sized meetings (20 to 50 people) and</li>
+							<li>number of small meetings (less than 20 people).</li>
+						</ul>
+
+						<p>
+							Any events held at the national level that contribute to DARIAH in your country should
+							be counted. This includes presentations about DARIAH to raise awareness of it and its
+							activities, or workshops and conferences dedicated to DARIAH. Please take into account
+							both face-to-face and virtual meetings. Note that the number you provide will be used
+							to help determine your in-kind contributions as well.
+						</p>
+
+						<h3>DARIAH Commissioned Event</h3>
+
+						<p>
+							These are big, international events that are commissioned by DARIAH and serve to
+							fulfill a strategic interest. Examples include the Annual Event or the DARIAH
+							Innovation Forum. DARIAH staff will fill this section out for you, if relevant.
+						</p>
 					</FormDescription>
 
 					<EventReportForm
@@ -194,11 +230,12 @@ async function DashboardCountryReportEditStepPageContent(
 				<section className="grid gap-6">
 					<FormTitle>{t("institutions")}</FormTitle>
 					<FormDescription>
-						Tempor aliqua non ad sint sint officia Lorem cupidatat cillum sint non tempor quis qui.
-						Voluptate sunt cillum et do cillum ut deserunt aliqua nisi pariatur velit adipisicing
-						sint. Incididunt laboris tempor cupidatat tempor laborum nulla consectetur consectetur
-						exercitation tempor veniam ullamco ullamco commodo. Nulla commodo ad dolor laboris nulla
-						tempor labore.
+						<p>
+							This response will consist of a single number and a list of your Partner Institutions
+							(including your National Coordinating Institution). Please verify that the information
+							below is up-to-date. If not, please give any corrections needed here. This list will
+							be used to update the DARIAH website &quot;members and partners&quot; section.
+						</p>
 					</FormDescription>
 
 					<InstitutionsForm
@@ -242,11 +279,10 @@ async function DashboardCountryReportEditStepPageContent(
 				<section className="grid gap-6">
 					<FormTitle>{t("project-funding-leverage")}</FormTitle>
 					<FormDescription>
-						Tempor aliqua non ad sint sint officia Lorem cupidatat cillum sint non tempor quis qui.
-						Voluptate sunt cillum et do cillum ut deserunt aliqua nisi pariatur velit adipisicing
-						sint. Incididunt laboris tempor cupidatat tempor laborum nulla consectetur consectetur
-						exercitation tempor veniam ullamco ullamco commodo. Nulla commodo ad dolor laboris nulla
-						tempor labore.
+						Please report the overall amount in Euros of any grant funding awarded to or within the
+						DARIAH national consortium that is directly or indirectly related to DARIAH activities.
+						Do not include funding from DARIAH itself (such as DARIAH Theme or Working Group
+						funding).
 					</FormDescription>
 
 					<ProjectsFundingLeveragesForm
@@ -265,11 +301,37 @@ async function DashboardCountryReportEditStepPageContent(
 				<section className="grid gap-6">
 					<FormTitle>{t("publications")}</FormTitle>
 					<FormDescription>
-						Tempor aliqua non ad sint sint officia Lorem cupidatat cillum sint non tempor quis qui.
-						Voluptate sunt cillum et do cillum ut deserunt aliqua nisi pariatur velit adipisicing
-						sint. Incididunt laboris tempor cupidatat tempor laborum nulla consectetur consectetur
-						exercitation tempor veniam ullamco ullamco commodo. Nulla commodo ad dolor laboris nulla
-						tempor labore.
+						<p>
+							The DARIAH definition of a publication is a significant and durable, accessible
+							contribution to knowledge. A publication counts as DARIAH-affiliated when:
+						</p>
+
+						<ul>
+							<li>
+								it resulted from work in the national consortium; funded by either DARIAH-EU or your
+								national funding for DARIAHs/ CLARIAH
+							</li>
+							<li>a DARIAH-affiliated tool or resource is cited in them;</li>
+							<li>it is about DARIAH/CLARIAH or</li>
+							<li>it is written by authors affiliated to DARIAH bodies or national DARIAHs.</li>
+						</ul>
+
+						<p>
+							They may be peer reviewed publications, or they may be reports, working papers,
+							pre-prints, major releases of training material or even substantive blog posts.
+							Importantly, please also include DOIs or other Persistent Identifiers (handles, HAL
+							IDs etc.) when reporting the bibliographical entries.
+						</p>
+
+						<p>
+							You also do not need to report items deposited into DARIAH-EU HAL or Zenodo
+							collections, as these are already automatically gathered.
+						</p>
+
+						<p>
+							To add your publications, please do so in your national folder of the DARIAH Zotero
+							Group Library following the Guidelines.
+						</p>
 					</FormDescription>
 
 					<PublicationsForm
@@ -310,11 +372,27 @@ async function DashboardCountryReportEditStepPageContent(
 				<section className="grid gap-6">
 					<FormTitle>{t("services")}</FormTitle>
 					<FormDescription>
-						Tempor aliqua non ad sint sint officia Lorem cupidatat cillum sint non tempor quis qui.
-						Voluptate sunt cillum et do cillum ut deserunt aliqua nisi pariatur velit adipisicing
-						sint. Incididunt laboris tempor cupidatat tempor laborum nulla consectetur consectetur
-						exercitation tempor veniam ullamco ullamco commodo. Nulla commodo ad dolor laboris nulla
-						tempor labore.
+						<p>
+							As a virtual digital research infrastructure, a large amount of service access takes
+							place over web-based platforms. This indicator attempts to capture the audience and
+							usage of DARIAH&apos;s national services.
+						</p>
+
+						<p>
+							Services in this list are the ones including in your national catalogue, as{" "}
+							<a href="https://marketplace.sshopencloud.eu/search?f.keyword=DARIAH+Resource">
+								maintained in the SSH Open Marketplace
+							</a>
+							, and displayed on the{" "}
+							<a href="https://www.dariah.eu/tools-services/tools-and-services/">
+								DARIAH service catalogue
+							</a>
+							. If you want to update the list, please refer to{" "}
+							<a href="https://drive.google.com/file/d/10tGdjKY8XC3TkNnVD_svl9_VrPoWTBWw/view">
+								these guidelines
+							</a>
+							.
+						</p>
 					</FormDescription>
 
 					<ServiceReportsForm
@@ -335,11 +413,24 @@ async function DashboardCountryReportEditStepPageContent(
 				<section className="grid gap-6">
 					<FormTitle>{t("software")}</FormTitle>
 					<FormDescription>
-						Tempor aliqua non ad sint sint officia Lorem cupidatat cillum sint non tempor quis qui.
-						Voluptate sunt cillum et do cillum ut deserunt aliqua nisi pariatur velit adipisicing
-						sint. Incididunt laboris tempor cupidatat tempor laborum nulla consectetur consectetur
-						exercitation tempor veniam ullamco ullamco commodo. Nulla commodo ad dolor laboris nulla
-						tempor labore.
+						<p>
+							List here any software/code of a specialized application that has been or is being
+							developed within your consortium and is made available under a permissive license for
+							reuse. Examples of software is any code made available through github (e.g.{" "}
+							<a href="https://github.com/acdh-oeaw/OpenAtlas">
+								https://github.com/acdh-oeaw/OpenAtlas
+							</a>
+							).
+						</p>
+
+						<p>
+							In distinction to services, which are available as web applications or APIs and can be
+							used directly, software needs to be downloaded and installed or executed on the side
+							of the user, to be used. The contribution should include the source code (and must be
+							documented as well, not just orphan code). The code can be in any programming
+							language, it can also be only a simple script dedicated to one specific task, as long
+							as it is working and documented.
+						</p>
 					</FormDescription>
 
 					<SoftwareForm comments={comments} countryId={country.id} reportId={report.id} />
@@ -354,21 +445,28 @@ async function DashboardCountryReportEditStepPageContent(
 				<section className="grid gap-6">
 					<FormTitle>{t("summary")}</FormTitle>
 					<FormDescription>
-						Tempor aliqua non ad sint sint officia Lorem cupidatat cillum sint non tempor quis qui.
-						Voluptate sunt cillum et do cillum ut deserunt aliqua nisi pariatur velit adipisicing
-						sint. Incididunt laboris tempor cupidatat tempor laborum nulla consectetur consectetur
-						exercitation tempor veniam ullamco ullamco commodo. Nulla commodo ad dolor laboris nulla
-						tempor labore.
+						<p>
+							Every DARIAH Member country must reach a certain threshold of contributions, set by
+							the DARIAH General Assembly. In the past, we asked National Coordinators to calculate
+							these sums by themselves. However, in line with the requests from National
+							Coordinators and the ongoing reforms to contributions, we have decided to offer
+							guidelines that allow In-Kind Contributions to be automatically calculated, following
+							input to the Unified National Report.
+						</p>
+
+						<p>
+							You have two options - either you have already calculated your total in-kind
+							contributions for your own purposes, or you can have us calculate them for you based
+							on the information reported in this Unified National Report. For further information,
+							please read the{" "}
+							<a href="https://docs.google.com/document/d/1A482x5XHwOsEZlmOcn5hw7lHy9muKMfeNs_T5BzQ4II/edit">
+								Policy on the financial value of DARIAH services and other IKCs
+							</a>{" "}
+							which explains two options:
+						</p>
 					</FormDescription>
 
 					<ReportSummary countryId={country.id} reportId={report.id} />
-
-					<div>
-						<span className="text-sm">Congrats, you will receive: </span>
-						<span className="text-lg font-semibold text-neutral-950 dark:text-neutral-0">
-							100.000.000.000 EUR
-						</span>
-					</div>
 				</section>
 			);
 		}
@@ -378,11 +476,13 @@ async function DashboardCountryReportEditStepPageContent(
 				<section className="grid gap-6">
 					<FormTitle>{t("welcome")}</FormTitle>
 					<FormDescription>
-						Tempor aliqua non ad sint sint officia Lorem cupidatat cillum sint non tempor quis qui.
-						Voluptate sunt cillum et do cillum ut deserunt aliqua nisi pariatur velit adipisicing
-						sint. Incididunt laboris tempor cupidatat tempor laborum nulla consectetur consectetur
-						exercitation tempor veniam ullamco ullamco commodo. Nulla commodo ad dolor laboris nulla
-						tempor labore.
+						<p>
+							Thank you in advance for your assistance in helping us to quantify and describe richly
+							some of the key aspects of DARIAH&apos;s operations as a distributed research
+							infrastructure. If you have any questions about what should be reported or how, please
+							do not hesitate to ask via{" "}
+							<Link href={createHref({ pathname: "/contact" })}>our contact form</Link>.
+						</p>
 					</FormDescription>
 
 					<Navigation code={code} next="institutions" year={year} />
