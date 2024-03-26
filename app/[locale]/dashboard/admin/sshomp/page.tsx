b91894f23caaf313dba8f3ca9b02749ea13d1d8b
stefanprobst <stefanprobst@posteo.de>
2024-03-26 14:19:52 +0100

    feat: add dashboard button to ingest data from ssh open marketplace (#98)
    this adds a new admin dashboard page to ingest software and services
    from the ssh open marketplace.
    
    note that for this to work, sshomp actor ids need to be associated with
    countries in the unr database. this is what the
    `prisma/create-country-ids-from-sshomp.ts` script does.

---
+++ app/[locale]/dashboard/admin/sshomp/page.tsx
@@ -0,0 +1,58 @@
+import type { Metadata, ResolvingMetadata } from "next";
+import { useTranslations } from "next-intl";
+import { getTranslations, unstable_setRequestLocale as setRequestLocale } from "next-intl/server";
+import type { ReactNode } from "react";
+
+import { AdminSshompIngestFormContent } from "@/components/admin/sshomp-ingest-form-content";
+import { MainContent } from "@/components/main-content";
+import { PageTitle } from "@/components/page-title";
+import type { Locale } from "@/config/i18n.config";
+
+interface DashboardAdminSshompIngestPageProps {
+	params: {
+		locale: Locale;
+	};
+}
+
+export async function generateMetadata(
+	props: DashboardAdminSshompIngestPageProps,
+	_parent: ResolvingMetadata,
+): Promise<Metadata> {
+	const { params } = props;
+
+	const { locale } = params;
+	const t = await getTranslations({ locale, namespace: "DashboardAdminSshompIngestPage" });
+
+	const metadata: Metadata = {
+		title: t("meta.title"),
+	};
+
+	return metadata;
+}
+
+export default function DashboardAdminSshompIngestPage(
+	props: DashboardAdminSshompIngestPageProps,
+): ReactNode {
+	const { params } = props;
+
+	const { locale } = params;
+	setRequestLocale(locale);
+
+	const t = useTranslations("DashboardAdminSshompIngestPage");
+
+	return (
+		<MainContent className="container grid content-start gap-y-8 py-8">
+			<PageTitle>{t("title")}</PageTitle>
+
+			<DashboardAdminSshompIngestContent />
+		</MainContent>
+	);
+}
+
+function DashboardAdminSshompIngestContent() {
+	return (
+		<section>
+			<AdminSshompIngestFormContent />
+		</section>
+	);
+}
---
+++ components/admin/sshomp-ingest-form-content.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import type { ReactNode } from "react";
+import { useFormState } from "react-dom";
+
+import { SubmitButton } from "@/components/submit-button";
+import { Form } from "@/components/ui/form";
+import { FormError as FormErrorMessage } from "@/components/ui/form-error";
+import { FormSuccess as FormSuccessMessage } from "@/components/ui/form-success";
+import { ingestDataFromSshompAction } from "@/lib/actions/sshomp";
+import { createKey } from "@/lib/create-key";
+
+export function AdminSshompIngestFormContent(): ReactNode {
+	const [formState, formAction] = useFormState(ingestDataFromSshompAction, undefined);
+
+	return (
+		<Form
+			action={formAction}
+			className="grid gap-y-6"
+			validationErrors={formState?.status === "error" ? formState.fieldErrors : undefined}
+		>
+			<SubmitButton>Ingest</SubmitButton>
+
+			<FormSuccessMessage key={createKey("form-success", formState?.timestamp)}>
+				{formState?.status === "success" && formState.message.length > 0 ? formState.message : null}
+			</FormSuccessMessage>
+
+			<FormErrorMessage key={createKey("form-error", formState?.timestamp)}>
+				{formState?.status === "error" && formState.formErrors.length > 0
+					? formState.formErrors
+					: null}
+			</FormErrorMessage>
+		</Form>
+	);
+}
