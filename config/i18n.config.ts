fdc52695ebfcef65b1af9868ec22ece47cb7ab72
Stefan Probst <stefanprobst@posteo.de>
2024-03-27 09:07:14 +0100

    fix: remove german locale

--- config/i18n.config.ts
+++ config/i18n.config.ts
@@ -1,7 +1,6 @@
-import type de from "@/messages/de.json";
 import type en from "@/messages/en.json";
 
-export const locales = ["de", "en"] as const;
+export const locales = ["en"] as const;
 
 export type Locale = (typeof locales)[number];
 
@@ -12,6 +11,5 @@ export function isValidLocale(value: string): value is Locale {
 }
 
 export interface Translations extends Record<Locale, IntlMessages> {
-	de: typeof de;
 	en: typeof en;
 }
--- lib/i18n.ts
+++ lib/i18n.ts
@@ -8,11 +8,12 @@ import { isValidLocale } from "@/config/i18n.config";
 export default getRequestConfig(async ({ locale }) => {
 	if (!isValidLocale(locale)) notFound();
 
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unnecessary-condition
 	const _messages = await (locale === "en"
 		? /** Enables hot-module-reloading for `en` when using `turbopack`. */
 			import("@/messages/en.json")
-		: import(`@/messages/${locale}.json`));
+		: // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+			import(`@/messages/${locale}.json`));
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 	const messages = _messages.default as IntlMessages;
 	const timeZone = "UTC";
