80fdb3a2789031ef838489f59e54ecddb41bd3b9
stefanprobst <stefanprobst@posteo.de>
2024-04-20 08:41:56 +0200

    feat: add outreach admin dashboard page (#126)
    this adds a new admin dashboard page to create, edit, and delete
    outreach entries.

---
+++ app/[locale]/dashboard/admin/outreach/page.tsx
@@ -0,0 +1,68 @@
+import type { Metadata, ResolvingMetadata } from "next";
+import { useTranslations } from "next-intl";
+import { getTranslations, unstable_setRequestLocale as setRequestLocale } from "next-intl/server";
+import { type ReactNode, Suspense } from "react";
+
+import { AdminOutreachTableContent } from "@/components/admin/outreach-table-content";
+import { MainContent } from "@/components/main-content";
+import { PageTitle } from "@/components/page-title";
+import type { Locale } from "@/config/i18n.config";
+import { getCountries } from "@/lib/data/country";
+import { getOutreach } from "@/lib/data/outreach";
+
+interface DashboardAdminOutreachPageProps {
+	params: {
+		locale: Locale;
+	};
+}
+
+export async function generateMetadata(
+	props: DashboardAdminOutreachPageProps,
+	_parent: ResolvingMetadata,
+): Promise<Metadata> {
+	const { params } = props;
+
+	const { locale } = params;
+	const t = await getTranslations({ locale, namespace: "DashboardAdminOutreachPage" });
+
+	const metadata: Metadata = {
+		title: t("meta.title"),
+	};
+
+	return metadata;
+}
+
+export default function DashboardAdminOutreachPage(
+	props: DashboardAdminOutreachPageProps,
+): ReactNode {
+	const { params } = props;
+
+	const { locale } = params;
+	setRequestLocale(locale);
+
+	const t = useTranslations("DashboardAdminOutreachPage");
+
+	return (
+		<MainContent className="container grid !max-w-screen-2xl gap-y-8 py-8">
+			<PageTitle>{t("title")}</PageTitle>
+
+			<DashboardAdminOutreachPageContent />
+		</MainContent>
+	);
+}
+
+function DashboardAdminOutreachPageContent() {
+	return (
+		<section>
+			<Suspense>
+				<AdminOutreachTable />
+			</Suspense>
+		</section>
+	);
+}
+
+async function AdminOutreachTable() {
+	const [countries, outreach] = await Promise.all([getCountries(), getOutreach()]);
+
+	return <AdminOutreachTableContent countries={countries} outreach={outreach} />;
+}
--- lib/data/outreach.ts
+++ lib/data/outreach.ts
@@ -2,6 +2,21 @@ import type { Country, Outreach } from "@prisma/client";
 
 import { db } from "@/lib/db";
 
+export function getOutreach() {
+	return db.outreach.findMany({
+		orderBy: {
+			name: "asc",
+		},
+		include: {
+			country: {
+				select: {
+					id: true,
+				},
+			},
+		},
+	});
+}
+
 interface GetOutreachByCountryParams {
 	countryId: Country["id"];
 }
@@ -84,6 +99,86 @@ export function createOutreach(params: CreateOutreachParams) {
 			name,
 			url,
 			type,
+		},
+	});
+}
+
+interface DeleteOutreachParams {
+	id: Outreach["id"];
+}
+
+export function deleteOutreach(params: DeleteOutreachParams) {
+	const { id } = params;
+
+	return db.outreach.delete({
+		where: {
+			id,
+		},
+	});
+}
+
+interface UpdateOutreachParams {
+	id: Outreach["id"];
+	name: Outreach["name"];
+	type: Outreach["type"];
+	url: Outreach["url"];
+	country?: string;
+	startDate?: Outreach["startDate"];
+	endDate?: Outreach["endDate"];
+}
+
+export function updateOutreach(params: UpdateOutreachParams) {
+	const { id, name, type, url, country, startDate, endDate } = params;
+
+	return db.outreach.update({
+		where: {
+			id,
+		},
+		data: {
+			name,
+			type,
+			url,
+			startDate,
+			endDate,
+			country:
+				country != null
+					? {
+							connect: {
+								id: country,
+							},
+						}
+					: undefined,
+		},
+	});
+}
+
+interface CreateFullOutreachParams {
+	name: Outreach["name"];
+	type: Outreach["type"];
+	url: Outreach["url"];
+	country?: string;
+	startDate?: Outreach["startDate"];
+	endDate?: Outreach["endDate"];
+}
+
+export function createFullOutreach(params: CreateFullOutreachParams) {
+	const { name, type, url, country, startDate, endDate } = params;
+
+	return db.outreach.create({
+		data: {
+			name,
+			type,
+			url,
+			startDate,
+			endDate,
+			country:
+				country != null
+					? {
+							connect: {
+								id: country,
+							},
+						}
+					: undefined,
 		},
 	});
 }
