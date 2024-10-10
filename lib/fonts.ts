47022566a53e0b7306277210f144479769c02cd7
Stefan Probst <stefanprobst@posteo.de>
2024-10-10 11:26:58 +0200

    build: update next and prisma, bump node to v22

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,15 +1,15 @@
 import { Inter } from "next/font/google";
 
 export const body = Inter({
-	axes: ["slnt"],
 	display: "swap",
+	style: ["normal", "italic"],
 	subsets: ["latin", "latin-ext"],
 	variable: "--font-body",
 });
 
 export const heading = Inter({
-	axes: ["slnt"],
 	display: "swap",
+	style: ["normal", "italic"],
 	subsets: ["latin", "latin-ext"],
 	variable: "--font-heading",
 });
