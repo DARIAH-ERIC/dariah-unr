d611240505b021d75568646187ca3900562ce713
Stefan Probst <stefanprobst@posteo.de>
2025-07-23 09:58:39 +0200

    fix: improve date conversion

--- lib/to-date-value.ts
+++ lib/to-date-value.ts
@@ -1,11 +1,14 @@
+import { assert } from "@acdh-oeaw/lib";
 import { type CalendarDate, parseDate } from "@internationalized/date";
 
 export function toDateValue(value: string | Date): CalendarDate {
 	const date = new Date(value);
 
-	const year = date.getFullYear();
-	const month = String(date.getMonth() + 1).padStart(2, "0");
-	const day = String(date.getDate()).padStart(2, "0");
+	assert(!Number.isNaN(date.getTime()), "Invalid Date");
+
+	const year = String(date.getUTCFullYear()).padStart(4, "0");
+	const month = String(date.getUTCMonth() + 1).padStart(2, "0");
+	const day = String(date.getUTCDate()).padStart(2, "0");
 
 	return parseDate([year, month, day].join("-"));
 }
