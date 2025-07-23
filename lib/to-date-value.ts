8a3d4dbcd9b78ed33029d20892b961e042084a66
Stefan Probst <stefanprobst@posteo.de>
2025-07-23 10:10:41 +0200

    perf: avoid date parsing

--- lib/to-date-value.ts
+++ lib/to-date-value.ts
@@ -1,14 +1,10 @@
 import { assert } from "@acdh-oeaw/lib";
-import { type CalendarDate, parseDate } from "@internationalized/date";
+import { CalendarDate } from "@internationalized/date";
 
 export function toDateValue(value: string | Date): CalendarDate {
 	const date = new Date(value);
 
 	assert(!Number.isNaN(date.getTime()), "Invalid Date");
 
-	const year = String(date.getUTCFullYear()).padStart(4, "0");
-	const month = String(date.getUTCMonth() + 1).padStart(2, "0");
-	const day = String(date.getUTCDate()).padStart(2, "0");
-
-	return parseDate([year, month, day].join("-"));
+	return new CalendarDate(date.getUTCFullYear(), date.getUTCMonth() + 1, date.getUTCDate());
 }
