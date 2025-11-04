600547d76d77c3ca2f2ab2aaf0a814519b1ee20b
Stefan Probst <stefanprobst@posteo.de>
2025-11-04 09:47:57 +0100

    chore: remove tests mocks

--- test/lib/db/mock.ts
+++
@@ -1,9 +0,0 @@
-import type { PrismaClient } from "@prisma/client";
-import { afterEach } from "vitest";
-import { mockDeep, mockReset } from "vitest-mock-extended";
-
-afterEach(() => {
-	mockReset(db);
-});
-
-export const db = mockDeep<PrismaClient>();
