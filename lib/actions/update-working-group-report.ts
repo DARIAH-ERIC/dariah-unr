42565003281dee54b9514991298aa57fa63603f0
Stefan Probst <stefanprobst@posteo.de>
2026-01-08 09:32:41 +0100

    feat: add working group report form (#341)
    closes #334
    
    this adds a working group report form.
    
    TODO:
    
    - [ ] decide where and how to link to this form.
    - [ ] confirmation button (for wg-chairs)

---
+++ lib/actions/update-working-group-report.ts
@@ -0,0 +1,82 @@
+"use server";
+
+import { log } from "@acdh-oeaw/lib";
+import { revalidatePath } from "next/cache";
+import { getTranslations } from "next-intl/server";
+import { z } from "zod";
+
+import { updateWorkingGroupReport } from "@/lib/data/working-group-report";
+import { getFormData } from "@/lib/get-form-data";
+import { assertAuthenticated } from "@/lib/server/auth/assert-authenticated";
+
+const formSchema = z.object({
+	workingGroupReportId: z.string(),
+	facultativeQuestions: z.string(),
+	narrativeReport: z.string(),
+});
+
+type FormSchema = z.infer<typeof formSchema>;
+
+interface FormReturnValue {
+	timestamp: number;
+}
+
+interface FormErrors extends FormReturnValue, z.typeToFlattenedError<FormSchema> {
+	status: "error";
+}
+
+interface FormSuccess extends FormReturnValue {
+	status: "success";
+	message: string;
+}
+
+type FormState = FormErrors | FormSuccess;
+
+export async function updateWorkingGroupReportAction(
+	previousFormState: FormState | undefined,
+	formData: FormData,
+): Promise<FormState> {
+	const t = await getTranslations("actions.updateWorkingGroupReport");
+
+	await assertAuthenticated();
+
+	const input = getFormData(formData);
+	const result = formSchema.safeParse(input);
+
+	if (!result.success) {
+		log.error(result.error.flatten());
+
+		return {
+			status: "error" as const,
+			...result.error.flatten(),
+			timestamp: Date.now(),
+		};
+	}
+
+	try {
+		const { workingGroupReportId, facultativeQuestions, narrativeReport } = result.data;
+
+		await updateWorkingGroupReport({
+			facultativeQuestions,
+			narrativeReport,
+			workingGroupReportId,
+		});
+
+		revalidatePath("/[locale]/dashboard/working-groups/[slug]/reports/[year]/edit", "page");
+
+		return {
+			status: "success" as const,
+			timestamp: Date.now(),
+			message: t("success"),
+		};
+	} catch (error) {
+		log.error(error);
+
+		return {
+			status: "error" as const,
+			formErrors: [t("errors.default")],
+			fieldErrors: {},
+			timestamp: Date.now(),
+		};
+	}
+}
--- lib/data/working-group-report.ts
+++ lib/data/working-group-report.ts
@@ -25,3 +25,55 @@ export function createReportForWorkingGroupId(params: CreateReportForWorkingGrou
 		},
 	});
 }
+
+interface GetWorkingGroupReportsByWorkingGroupIdParams {
+	workingGroupId: string;
+}
+
+export function getWorkingGroupReportsByWorkingGroupId(
+	params: GetWorkingGroupReportsByWorkingGroupIdParams,
+) {
+	const { workingGroupId } = params;
+
+	return db.workingGroupReport.findMany({
+		where: {
+			workingGroupId,
+		},
+	});
+}
+
+interface GetWorkingGroupReportParams {
+	workingGroupId: string;
+	year: number;
+}
+
+export function getWorkingGroupReport(params: GetWorkingGroupReportParams) {
+	const { workingGroupId, year } = params;
+
+	return db.workingGroupReport.findFirst({
+		where: {
+			workingGroupId,
+			year,
+		},
+	});
+}
+
+interface UpdateWorkingGroupReportParams {
+	facultativeQuestions: string;
+	narrativeReport: string;
+	workingGroupReportId: string;
+}
+
+export function updateWorkingGroupReport(params: UpdateWorkingGroupReportParams) {
+	const { facultativeQuestions, narrativeReport, workingGroupReportId } = params;
+
+	return db.workingGroupReport.update({
+		where: {
+			id: workingGroupReportId,
+		},
+		data: {
+			facultativeQuestions,
+			narrativeReport,
+		},
+	});
+}
